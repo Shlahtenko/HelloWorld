7ae7f738c21737dc5584c5f3faeb850227842708
Shlahtenko <Shlahtenko2051@ukr.net>
2021-11-20 12:49:47 +0200

    urok

---
+++ 04/app.py
@@ -0,0 +1,5 @@
+l = [1, 4, True, 'nrjs', ]
+length = len(l)
+
+for i in range(0, length):
+    print(l[i])
---
+++ main.py
@@ -0,0 +1,112 @@
+# def change_file_encoding(
+#         in_file_name: str,
+#         in_encoding: str,
+#         out_encoding: str,
+#         out_file_name: str,
+# ):
+#     open(in_file_name)
+
+# with open('C:/Users/a512s03\PycharmProjects/N2/venv/test.txt', 'w') as file:
+#     file.write('a')
+
+# s = 'nfdfvsfsvvsv ds'
+
+# in_file = open()
+# out_file = open()
+#
+# for a in s:
+#     print(a)
+#
+# in_file.close()
+# out.file.close()
+
+# with open('C:/Users/a512s03/PycharmProjects/N2/venv/test.txt', 'r', encoding='') as in_file_data:
+#     content = file.read()
+#     print(content)
+
+# a = input('Сколько мне лет')
+#
+# b = int(a)
+
+# class wheel:
+#     def __init__(self, a: bool):
+#         self.is_have_spitsa = a
+#
+# a = wheel('whell: bool')
+# a = wheel(True)
+# print(a)
+
+# class wheel:
+#     def __init__(self):
+
+# with open('C:/Users/a512s03/PycharmProjects/N2/venv/test.txt', 'r+', encoding='CP 875') as in_file_data:
+#     content = file.r+()
+#     print(content)
+
+# l = [1, 4, 3, 7, ]
+#
+# l.sort()
+# l.append(5)
+# l.remove(3)
+# print(l)
+
+# t = tuple((1, 4, 6, 78))
+#
+# [print(i) for i in t]
+
+# s = {1, 4, 6, 6, 6}
+# s1 = {2, 3}
+#
+# # s.clear()
+# s.update(s1)
+#
+# print(s)
+
+# l = [1, 2, 3, 4]
+#
+# l1 = [10, 12, 13]
+#
+# l = l1[:]
+#
+# print()
+
+# d = {
+#     1: 'auto',
+#     2: 'True',
+# }
+# print(d.values())
+
+# l = [1, 2, 3, 5, 6]
+#
+# a = 5
+# b = 10
+#
+# a, *b = l
+#
+# print(a, b)
+# print(*l)
+#
+# def a(*args):
+#     print(*args)
+# a(1,3, 5, 6)
+
+# d = {
+#     1: '01',
+#     2: '02',
+#     3: '03'
+# }
+#
+# for key, value in d.items():
+#     print(f'{key=}, {value=}')
+
+# class bike:
+#     def __init__(self):
+#         self.wheel_count = 2
+#         self.length = 10
+#
+#     def __str__(self) -> str:
+#         return f'{self.wheel_count=} {self.length=}'
+#
+# a = bike()
+#
+# print(a.__dict__.keys())
